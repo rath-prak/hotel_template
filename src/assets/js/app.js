9437859dafd20024e76858efb12c7c13821d36be
rath-prak <rathanap@gmail.com>
2018-01-17 15:54:36 +1300

    fix overlay navigation

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,12 +7,17 @@ window.$ = $;
 
 
 $(document).ready(function(){
+
+  $('button').on('click', function() {
+    $(this).toggleClass('is-active');
+  });
+
   let $menuToggle = $('.menu-toggle');
-  let $overlayMenu = $('.overlay-menu');
+  let $overlayMenuWrapper = $('.overlay-menu-wrapper');
 
   const tl = new TimelineMax({ delay:3, paused:true, reversed:true });
 
-  tl.to($overlayMenu, 1, { y: '0', ease: Power2.easeOut });
+  tl.to($overlayMenuWrapper, 1, { y: '0', ease: Power2.easeOut });
 
   $menuToggle.on('click', () => {
     setTimeout(function(){
